bd55a7dac95ecd29826c9ceb5333331e1a498d7b
jharter1 <jharter1@users.noreply.github.com>
2020-07-27 11:35:13 -0500

    Create README.ml
    adding a readme for additional instructions.

---
+++ README.ml
@@ -0,0 +1,15 @@
+# This is K8s on Vagrant
+
+This is a quick script that lets you start up a k8s cluster on any old Ubuntu box you may have had lying around.
+Mine has 8 GB RAM and a 4-core i5 processor, so your results may vary.
+[Credit to original instructions that I modified are here.](https://kubernetes.io/blog/2019/03/15/kubernetes-setup-using-ansible-and-vagrant/)
+
+### Instructions for use
+
+Install Ansible and Vagrant and confirm they work using the ```--version``` flag
+
+Clone this project
+
+CD into the cloned dir
+
+```vagrant up && vagrant ssh k8s-master```
